99120339bf99a54edff8f84cab4233671ca9e721
l-donovan <ladbuilder1@gmail.com>
2015-08-01 20:37:14 -0400

    Deprecated 'config.js', more logical structure

--- content/databases/frontPage.js
+++ content/databases/frontPage.js
@@ -40,4 +40,4 @@ items.push({
       controls: true,
     }
   }
-})
+});
---
+++ js/jsfiller.js
@@ -0,0 +1,16 @@
+function jsfiller_load(db, loc) {
+  items = [];
+  $.getScript(db, function() {
+    for (i = 0; i < items.length; i++) {
+      $.ajax({async: false, url: "/content/templates/" + items[i].type + ".html", success: function (html) {
+        tokens = html.match(/\{\$[^\{\$\}]+\$\}/g);
+        for (j = 0; j < tokens.length; j++) {
+          value = tokens[j].replace(/[\{\$\}]+/g, '');
+          item = items[i];
+          html = html.replace(tokens[j], eval(value));
+        }
+        $(loc).append(html);
+      }});
+    }
+  });
+}
