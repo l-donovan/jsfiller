20f22735cf0ee4953366a2b94d8a00c628d2c88e
Luke Donovan <ladbuilder1@gmail.com>
2015-08-31 09:11:26 -0400

    updated for gh-pages

--- js/jsfiller.js
+++ js/jsfiller.js
@@ -5,7 +5,7 @@ function jsfiller_load(db, loc) {
   $.getScript(db, function() {
     for (i = 0; i < items.length; i++) {
       items[i].id = id;
-      $.ajax({async: false, url: "/content/templates/" + items[i].type + ".html", success: function (html) {
+      $.ajax({async: false, url: "content/templates/" + items[i].type + ".html", success: function (html) {
         tokens = html.match(/\{\$[^\{\$\}]+\$\}/g);
         for (j = 0; j < tokens.length; j++) {
           value = tokens[j].replace(/[\{\$\}]+/g, '');
